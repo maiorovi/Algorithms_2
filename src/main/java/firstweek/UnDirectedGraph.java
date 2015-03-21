6ec27cd8320f9a67812ded6d84dbde847848460d
tryharder <maiorov.prog@gmail.com>
2015-03-21 20:53:36 +0200

    initial commit

---
+++ src/main/java/firstweek/UnDirectedGraph.java
@@ -0,0 +1,29 @@
+package firstweek;
+
+import java.io.InputStream;
+
+public class UnDirectedGraph {
+    private int v;
+    private int e;
+
+    public UnDirectedGraph(int V) {
+
+    }
+
+    public UnDirectedGraph(InputStream strem) {
+
+    }
+
+    public int v() {
+        return v;
+    }
+
+    public int e() {
+        return e;
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+
+}
