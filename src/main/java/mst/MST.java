e194919fcb97ca41c06200a4d8149a577b7f2e70
tryharder <maiorov.prog@gmail.com>
2015-03-29 20:51:10 +0300

    added MST and MinPQ classes

---
+++ src/main/java/mst/MST.java
@@ -0,0 +1,16 @@
+package mst;
+
+public class MST {
+
+    public MST(EdgeWeightedGraph graph) {
+
+    }
+
+    public Iterable<Edge> edges() {
+
+    }
+
+    public double weight() {
+        return 0.0;
+    }
+}
